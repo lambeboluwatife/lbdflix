fe81b7f9a20de1dffd92e5757c48bcb43064a813
lambeboluwatife <boluwatifelambe@gmail.com>
2024-02-07 15:35:16 +0100

    hero section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,13 +1,37 @@
-const Hero = ({ heroData }) => {
+"use client";
+import { useState } from "react";
+
+const Hero = ({ movies }) => {
+  // Destructure movie data from movies.results array
+  const [movieToDisplay, setMovieToDisplay] = useState(movies.results[0]);
+
   const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
   return (
     <div
       className="hero-section"
       style={{
-        backgroundImage: `url(${IMG_PATH}${heroData.backdrop_path})`,
+        backgroundImage: `url(${IMG_PATH}${movieToDisplay.backdrop_path})`,
       }}
     >
-      <h1>{heroData.title}</h1>
+      <div className="showcase">
+        {/* Wrap event handlers in arrow functions */}
+        <h1 onClick={() => setMovieToDisplay(movies.results[0])}>
+          {movies.results[0].title}
+        </h1>
+        <h1 onClick={() => setMovieToDisplay(movies.results[1])}>
+          {movies.results[1].title}
+        </h1>
+        <h1 onClick={() => setMovieToDisplay(movies.results[2])}>
+          {movies.results[2].title}
+        </h1>
+        <h1 onClick={() => setMovieToDisplay(movies.results[3])}>
+          {movies.results[3].title}
+        </h1>
+        <h1 onClick={() => setMovieToDisplay(movies.results[4])}>
+          {movies.results[4].title}
+        </h1>
+      </div>
     </div>
   );
 };
