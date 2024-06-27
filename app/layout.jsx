51ace2092db709cf67c8806be355b4fdb4a4b413
ayodeji <ayodejiakintobi1@gmail.com>
2024-06-27 19:44:14 +0100

    UPDATE: INSTALLED SASS AND IMPORT

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,9 @@
 import { Poppins } from "next/font/google";
 
-import "./css/style.css";
+// import "./css/style.css";
+import "../scss/style.scss";
 import ScrollAwareNavbar from "./components/ScrollAwareNavbar";
 import Footer from "./components/Footer";
-
 
 const poppins = Poppins({
   weight: ["400", "700"],
