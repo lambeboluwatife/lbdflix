370756aa641152fba0e138efc52073642cda6f1f
lambeboluwatife <boluwatifelambe@gmail.com>
2024-11-20 09:48:34 +0100

    user signin

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,7 +3,21 @@ import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
